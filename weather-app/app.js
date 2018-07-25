7b0ea9a481774dd1b1cc589693f6a4f5c9fa7069
Eddy Vinck <eddyvinck95@gmail.com>
2018-07-25 21:05:35 +0200

    Refactor using Axios, a promise based http client

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,5 @@
 const yargs = require('yargs');
-
-const geocode = require('./geocode/geocode.js');
-const weather = require('./weather/weather.js');
+const axios = require('axios');
 
 const argv = yargs
   .options({
@@ -11,34 +9,86 @@ const argv = yargs
       describe: 'Address to fetch weather for',
       string: true,
     },
-    d: {
-      alias: 'debug',
-      describe: 'Display extra information for debugging',
-      boolean: true,
-    },
   })
   .help()
   .alias('help', 'h').argv;
 
-if (argv.d) {
-  console.log(argv);
-}
+const geocodeKey = 'AIzaSyC8KhY00La7zp9Q0QPKwlNkRAkas_P9awM';
+const encodedAddress = encodeURIComponent(argv.address);
 
-geocode
-  .geocodeAddress(argv.address)
-  .then(weather.getWeather)
+const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${geocodeKey}`;
+
+const getWeatherUrl = (geolocation) => {
+  const API_KEY = '84424dfdd3d5ceb2d12742a00911c920';
+  const options = {
+    lang: 'en',
+    units: 'auto',
+  };
+  const { latitude, longitude, city, address } = geolocation;
+  const url = `https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}?`;
+
+  let optionParameters = '';
+
+  for (const option in options) {
+    if (options.hasOwnProperty(option)) {
+      optionParameters += `${option}=${options[option]}&`;
+    }
+  }
+  const weatherUrl = url + optionParameters;
+
+  return weatherUrl;
+};
+
+axios
+  .get(geocodeUrl)
+  .then((response) => {
+    if (response.data.status === 'ZERO_RESULTS') {
+      throw new Error('Address could not be found.');
+    }
+
+    const localityObject = response.data.results[0].address_components.filter((obj) => {
+      return obj.types.includes('locality');
+    })[0];
+    const city = localityObject.long_name;
+    const geometry = response.data.results[0].geometry;
+    const address = response.data.results[0].formatted_address;
+    const latitude = geometry.location.lat;
+    const longitude = geometry.location.lng;
+
+    const geoInfo = {
+      latitude,
+      longitude,
+      city,
+      address,
+    };
+    return geoInfo;
+  })
+  .then((geolocation) => {
+    return axios.get(getWeatherUrl(geolocation)).then((response) => {
+      return {
+        geolocation,
+        response,
+      };
+    });
+  })
   .then((results) => {
-    let weatherMessage = `It is currently ${results.weather.currently.temperature} degrees in ${
-      results.city
-    }.`;
+    let weatherMessage = `It is currently ${results.response.data.currently.temperature} degrees.`;
 
-    if (results.weather.currently.temperature !== results.weather.currently.apparentTemperature) {
+    if (
+      results.response.data.currently.temperature !==
+      results.response.data.currently.apparentTemperature
+    ) {
       weatherMessage += ` However, it feels like ${
-        results.weather.currently.apparentTemperature
+        results.response.data.currently.apparentTemperature
       } degrees.`;
     }
     console.log(weatherMessage);
   })
-  .catch((error) => {
-    console.log(error);
+  .catch((e) => {
+    console.log(e);
+    if (e.code === 'ENOTFOUND') {
+      console.log('Could not connect to geolocation server.');
+    } else {
+      console.log(e.message);
+    }
   });
